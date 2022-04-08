9750f048f3311fdbe309a84435971583c0b76b41
Mustafa Arınmış <mustafa.arinmis@linarit.com>
2022-04-08 23:20:17 +0300

    Add time and location picker

--- src/components/date_location_picker.js
+++ src/components/date_location_picker.js
@@ -1,3 +1,84 @@
-const DateLocationPicker = () => {};
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+const DateLocationPicker = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  return (
+    <div className="grid gap-4 grid-rows-3">
+      <div className="grid gap-4 sm:grid-cols-2 grid-cols-1">
+        <div className="flex items-center">
+          <label
+            for="pickupOffice"
+            class="block mb-2 border-l-4 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Pick-up Office
+          </label>
+          <input
+            type="text"
+            name="pickupOffice"
+            id="pickupOffice"
+            placeholder="Antalya"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+          />
+        </div>
+        <div className="flex">
+          <label
+            for="pickupDate"
+            class="block mb-2 border-l-4 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Pick-up Date
+          </label>
+          <DatePicker
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+        </div>
+      </div>
+
+      <div className="grid gap-4 sm:grid-cols-2 grid-cols-1">
+        <div className="flex">
+          <label
+            for="pickupOffice"
+            class="block mb-2 border-l-4 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Return Office
+          </label>
+          <input
+            type="text"
+            name="pickupOffice"
+            id="pickupOffice"
+            placeholder="Antalya"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+          />
+        </div>
+        <div className="flex">
+          <label
+            for="pickupDate"
+            class="block mb-2 border-l-4 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Return Date
+          </label>
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+          />
+        </div>
+      </div>
+      <div className="flex justify-end items-center">
+        <button
+          onClick={() => {}}
+          class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Rent
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default DateLocationPicker;
--- src/user/select_date/select_date.js
+++ src/user/select_date/select_date.js
@@ -1,7 +1,15 @@
-import NavbarUser from "../../components/navbar_user";
+import DateLocationPicker from "../../components/date_location_picker";
 
 const SelectDate = () => {
-  return <NavbarUser />;
+  return (
+    <>
+      <div className="m-10 sm:m-40  flex justify-center items-center ">
+        <div className="divide-y p-10 pb-4 space-y-6 lg:px-8 sm:pb-6 xl:pb-8 block  w-fit bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <DateLocationPicker />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SelectDate;
