9c56b11693ab082be8e2eb155f4c7d1aa9b3cf72
arinmis <mustafa_arinmis@outlook.com>
2022-05-12 18:06:49 +0300

    Impelement logout

--- src/store/AuthContext.js
+++ src/store/AuthContext.js
@@ -5,7 +5,8 @@ const AuthContext = createContext();
 export default AuthContext;
 
 const useAuth = () => {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(
+    localStorage.getItem("access") ? true : false);
   return { isAuth, setIsAuth };
 };
 export const AuthProvider = ({ children }) => {
