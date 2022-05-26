e163c752d5e2487de64ef46096fd9c013f6288ed
Mustafa Arınmış <mustafa.arinmis@linarit.com>
2022-05-26 21:31:42 +0300

    Store selected car information

--- src/user/select_car/select_car.js
+++ src/user/select_car/select_car.js
@@ -1,28 +1,57 @@
 import CarFilter from "../../components/car_filter";
 import Car from "../../components/car";
-import MockCar from "../../assets/mock_car";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import RentContext from "../../store/RentContext";
 
 const SelectCar = (props) => {
+  const { rent, setRent } = useContext(RentContext);
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    axios.get("car/").then((response) => {
+      console.log(response.data);
+      setCars(response.data);
+    });
+  }, []);
   /* create mock cars */
-  const goNextStep = () => {
-    props.setStep(2);
+
+  const getCars = () => {
+    if (cars.length === 0) return "Oppps, no car to show";
+
+    return cars.map((car) => {
+      const features = [
+        `Daily price: ${car.daily_price}$`,
+        `${car.transmission_type.transmission_type}`,
+        `${car.fuel_type.fuel_type}`,
+      ];
+
+      const goNextStep = () => {
+        setRent({
+          ...rent,
+          car_id: car.id,
+        });
+        props.setStep(2);
+      };
+
+      const rentCarButton = (
+        <button onClick={goNextStep} className="btn-primary ml-5">
+          Rent
+        </button>
+      );
+
+      return (
+        <Car
+          key={car.id}
+          id={car.id}
+          carImage={car.photo}
+          carName={car.brand_type.brand_type}
+          features={features}
+          actions={[rentCarButton]}
+        />
+      );
+    });
   };
-
-  const rentCarButton = (
-    <button onClick={goNextStep} className="btn-primary ml-5">
-      Rent
-    </button>
-  );
-
-  const carAmount = 10;
-  const cars = Array.from(Array(carAmount).keys()).map((item) => (
-    <Car
-      carImage={MockCar.carImage}
-      carName={MockCar.carName}
-      features={MockCar.features}
-      actions={[rentCarButton]}
-    />
-  ));
 
   return (
     <div className="grid grid-cols-1  sm:grid-cols-3">
@@ -30,7 +59,7 @@ const SelectCar = (props) => {
         <CarFilter />
       </div>
       <div className="col-span-3 sm:col-span-2 ">
-        <div className="grid grid-row-1 gap-10 px-2 sm:px-10">{cars}</div>
+        <div className="grid grid-row-1 gap-10 px-2 sm:px-10">{getCars()}</div>
       </div>
     </div>
   );
