eeeb85c56e9214f221ad7722092b3dbd351b81a0
Mustafa Arınmış <mustafa.arinmis@linarit.com>
2022-05-27 23:59:38 +0300

    Implement cancel reservation

--- src/user/reservations/reservations.js
+++ src/user/reservations/reservations.js
@@ -7,8 +7,7 @@ import axios from "axios";
 const Reservations = () => {
   const navigate = useNavigate();
   const [reservationData, setReservationData] = useState([]);
-
-  useEffect(() => {}, []);
+  const [reservations, setReservations] = useState([]);
 
   useEffect(() => {
     axios.get("/reservation/").then((response) => {
@@ -16,6 +15,56 @@ const Reservations = () => {
     });
   }, []);
 
+  useEffect(() => {
+    setReservations(
+      reservationData.map((reservation) => {
+        const cancelButton = (
+          <button
+            onClick={() => {
+              removeReservation(reservation.id);
+              // setReservationData(reservationData);
+              // setReservationData([]);
+            }}
+            className="btn-primary ml-5"
+          >
+            Cancel
+          </button>
+        );
+
+        const features = [
+          `Reservation ID: ${reservation.id}`,
+          `${reservation.car.transmission_type.transmission_type}`,
+          `${reservation.car.fuel_type.fuel_type}`,
+          `Pick-up Location: ${reservation.pickup_location.city}`,
+          `Return Location: ${reservation.return_location.city}`,
+          `Pick-up Date: ${reservation.pickup_date}`,
+          `Return Date: ${reservation.return_date}`,
+          `Daily Price: ${reservation.car.daily_price} $`,
+          `Total Price: ${
+            reservation.car.daily_price *
+            differenceInDays(
+              new Date(reservation.return_date),
+              new Date(reservation.pickup_date)
+            )
+          } $`,
+        ];
+
+        return (
+          <Car
+            key={reservation.id}
+            id={reservation.id}
+            carImage={reservation.car.photo}
+            carName={reservation.car.brand_type.brand_type}
+            features={features}
+            cardColor={"bg-sky-100"}
+            actions={[cancelButton]}
+          />
+        );
+      })
+    );
+  }, [reservationData]);
+
+  /*
   const editButton = (
     <button
       onClick={() => {
@@ -27,56 +76,27 @@ const Reservations = () => {
       Edit
     </button>
   );
-
-  const cancelButton = (
-    <button
-      onClick={() => {
-        // setReservations([]);
-      }}
-      className="btn-primary ml-5"
-    >
-      Cancel
-    </button>
-  );
-
-  /*
-  <Car
-    carImage={MockCar.carImage}
-    carName={"Audi A3"}
-    features={allFeatures}
-    cardColor={"bg-sky-100"}
-    actions={[editButton, cancelButton]}
-  />
   */
-  const reservations = reservationData.map((reservation) => {
-    const features = [
-      `Reservation ID: ${reservation.id}`,
-      `${reservation.car.transmission_type.transmission_type}`,
-      `${reservation.car.fuel_type.fuel_type}`,
-      `Pick-up Location: ${reservation.pickup_location.city}`,
-      `Return Location: ${reservation.return_location.city}`,
-      `Pick-up Date: ${reservation.pickup_date}`,
-      `Return Date: ${reservation.return_date}`,
-      `Daily Price: ${reservation.car.daily_price} $`,
-      `Total Price: ${
-        reservation.car.daily_price *
-        differenceInDays(
-          new Date(reservation.return_date),
-          new Date(reservation.pickup_date)
-        )
-      } $`,
-    ];
 
-    return (
-      <Car
-        carImage={reservation.car.photo}
-        carName={reservation.car.brand_type.brand_type}
-        features={features}
-        cardColor={"bg-sky-100"}
-        actions={[editButton, cancelButton]}
-      />
-    );
-  });
+  const removeReservation = (id) => {
+    // find the reservation
+    let index = -1;
+    reservationData.forEach((reservation, i) => {
+      if (reservation.id === id) {
+        index = i;
+        return;
+      }
+    });
+    const removedReservation = reservationData.splice(index, 1)[0];
+    // remove from UI
+    setReservationData([...reservationData]); // copy to chage state
+    // remove from backend
+    console.log("removedReservation", removedReservation);
+    console.log("removedReservation", removedReservation.id);
+    axios.delete(`/reservation/${removedReservation.id}/`).then((response) => {
+      console.log(response.data);
+    });
+  };
 
   return (
     <div>
